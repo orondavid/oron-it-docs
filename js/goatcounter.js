b924a1cd13ba5491adb4dfde00c05880aec3986e
orondavid <davidoron7@gmail.com>
2025-06-25 19:05:48 +0300

    Deployed 27aa837 with MkDocs version: 1.6.1

--- js/goatcounter.js
+++ js/goatcounter.js
@@ -1,5 +1,12 @@
-let sc=document.createElement('script');
-sc.setAttribute('data-goatcounter','https://oron-it.goatcounter.com/count');
-sc.src='//gc.zgo.at/count.js';
-sc.async=true;
+// מונע קידוד כפול של כתובת העמוד (path)
+window.goatcounter = {
+    path: function() {
+        return decodeURIComponent(window.location.pathname);
+    }
+};
+
+let sc = document.createElement('script');
+sc.setAttribute('data-goatcounter', 'https://oron-it.goatcounter.com/count');
+sc.src = '//gc.zgo.at/count.js';
+sc.async = true;
 document.head.appendChild(sc);
