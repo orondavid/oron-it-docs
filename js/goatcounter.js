14993cb7688f2a3f9331a46bfd8d672d7a312bb3
orondavid <davidoron7@gmail.com>
2025-06-25 18:50:05 +0300

    Deployed 27aa837 with MkDocs version: 1.6.1

---
+++ js/goatcounter.js
@@ -0,0 +1,5 @@
+let sc=document.createElement('script');
+sc.setAttribute('data-goatcounter','https://oron-it.goatcounter.com/count');
+sc.src='//gc.zgo.at/count.js';
+sc.async=true;
+document.head.appendChild(sc);
