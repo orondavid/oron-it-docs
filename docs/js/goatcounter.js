3715b2ce2e6a317f9193840fd4cb9d2686bf953b
orondavid <62870368+orondavid@users.noreply.github.com>
2025-06-25 17:56:07 +0300

    Create goatcounter.js

---
+++ docs/js/goatcounter.js
@@ -0,0 +1,6 @@
+let sc = document.createElement('script');
+sc.setAttribute('data-goatcounter', 'https://davidoron7.goatcounter.com/count');
+sc.src = '//gc.zgo.at/count.js';
+sc.async = true;
+document.head.appendChild(sc);
+
